314e445b1e21866dd91d721191375a17165f4394
guavalines <konstrictor@gmail.com>
2023-01-03 22:44:03 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'restaurants#index'
-  resources :restaurants, only: [:index, :show, :new, :create, :update, :destroy]
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [ :index, :show, :new, :create ] do
+    resources :reviews, only: [:create]
+  end
 end
