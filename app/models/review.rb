ebda2a700dd4264def77eee61791c9b0078d6a5e
guavalines <konstrictor@gmail.com>
2023-01-03 22:38:51 -0600

    updated models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, numericality: true, inclusion: { in: 0..5 }
-  validates :content, presence: true
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
